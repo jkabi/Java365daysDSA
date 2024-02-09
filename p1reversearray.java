af7e1aec755b641bc6e03690b8a9373c2cdc7950
jkabi <kabilanu192205@gmail.com>
2024-02-09 08:37:35 +0530

    reverse array uploaded

--- p1reversearray.java
+++ p1reversearray.java
@@ -1,5 +1,18 @@
 public class p1reversearray {
     public static void main(String [] args){
-        System.out.println("skjldih");
-    }
+//    REVERSE ARRAY
+        int [] arr = new int []{12,34,56,78,89};
+        int temp;
+        int i=0;
+        int j =arr.length-1;
+        while (i<j){
+            temp = arr[i];
+            arr[j] = arr[i];
+            i++;
+            j--;
+        }
+        for(int k =0 ;k<arr.length;k++){
+            System.out.print(arr[k]+ " ");
+        }
+     }
 }
